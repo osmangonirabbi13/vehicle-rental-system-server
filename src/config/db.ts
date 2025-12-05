0e13b091bc95380cc906ac25c44a1ae2bd8d9653
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-12-05 19:26:01 +0600

    vehicles db create

--- src/config/db.ts
+++ src/config/db.ts
@@ -16,6 +16,16 @@ const initDB = async () => {
         role VARCHAR(10) NOT NULL 
         )
         `);
+  await pool.query(`
+    CREATE TABLE  IF NOT EXISTS Vehicles (
+    id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(255) NOT NULL,
+    type VARCHAR(10) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+    registration_number VARCHAR(50) NOT NULL UNIQUE,
+    daily_rent_price FLOAT  NOT NULL CHECK (daily_rent_price > 0),
+    availability_status VARCHAR(10) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+    );
+    `);
 };
 
 export default initDB;
